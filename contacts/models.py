d7673f5a6f879a28cb6cebfed7045dddfc5462f0
Michelle Baert <rocky.road@rocky-shore.net>
2014-07-08 23:41:43 +0200

    Contact email relationship and tests

--- contacts/models.py
+++ contacts/models.py
@@ -7,19 +7,32 @@ class Contact(CRUDMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80), nullable=True)
     last_name = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(80), nullable=False)
+    #email = db.Column(db.String(80), nullable=False)
+    #: @type emails sqlalchemy.orm.Query
     phone = db.Column(db.Text)
     address = db.Column(db.Text)
     date_of_birth = db.Column(db.Date)
     notes = db.Column(db.Text)
 
-    def __init__(self, first_name=None, last_name=None, email=None):
-        """
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
 
-        :param first_name:
-        :param last_name:
-        :param email:
-        """
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
+    # def set_email(self, email):
+    #     if isinstance(Email, email):
+    #         obj = email
+    #     else:
+    #         obj = Email.get_by_id(email)
+    #         if not obj:
+    #             obj = Email.create(email=email, contact_id = self.id)
+    #     self.emails = obj
+    #     return obj
+
+class Email(CRUDMixin, db.Model):
+    #__tablename__ = 'emails'
+
+    email = db.Column(db.String(80), primary_key=True)
+    contact_id = db.Column(db.Integer, db.ForeignKey('contact.id'))
+    # Relationship to Contact or any subtype instances
+    contact = db.relationship('Contact', enable_typechecks=False,
+                              backref=db.backref('emails', lazy='dynamic'),
+    )
--- contacts/tests.py
+++ contacts/tests.py
@@ -1,18 +1,19 @@
 from flask import url_for
+from contacts.models import Contact, Email
 from testing import KitTestCase
-
+#import sqlalchemy as sa
+import sqlalchemy.orm
 
 class ContactsTestCase(KitTestCase):
     """
     Put here utilities, not tests.
-
+ct
     """
 
-    # def db_addEntity(self, name="Sample", description="This is a sample for testing"):
-    #     "Adds a record directly to model and database"
-    #     entity = Entity(name, description)
-    #     entity.save()
-    #     return entity
+    def db_addEntity(self, first_name="John", last_name="Doe"):
+        "Adds a record directly to model and database"
+        entity = Contact.create(first_name=first_name, last_name=last_name)
+        return entity
 
 
 class TestContactsPages(ContactsTestCase):
@@ -26,3 +27,22 @@ class TestContactsPages(ContactsTestCase):
         response = self.client.get(url_for('contacts.index'))
         self.assert200(response)
 
+    def testJohnDoe(self):
+        doe = self.db_addEntity("John", "Doe")
+
+    def testEmail(self):
+        """
+
+
+        """
+        doe = self.db_addEntity("John", "Doe")
+        #: @type emails sqlalchemy.orm.Query
+        emails = doe.emails
+        self.assertEquals(0, emails.count())
+        email = Email.create(email='john.doe@example.com', contact=doe)
+        #doe.emails.append(email)
+        self.assertEquals(1, emails.count())
+        #self.assertEquals(email.id, emails.one().id)
+        self.assertEquals(email.email, emails.one().email)
+        Email.create(email='john.doe@hotmail.com', contact_id=doe.id)
+        self.assertEquals(2, emails.count())
